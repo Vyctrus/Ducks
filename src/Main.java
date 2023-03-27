46eee5b5be0f050e74174d681b8fc09e0fdc5de7
Vyctrus <and.gierlak@gmail.com>
2023-03-27 20:08:13 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,81 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void readExcelData(){
+        System.out.println("Hello");
+        int duckNumber=0;
+        int[][] ducks;
+        int rowSize=0;
+
+        try{
+            File myObj = new File("name_file.txt");
+            Scanner myReader = new Scanner(myObj);
+            if(myReader.hasNextLine()){
+                duckNumber= myReader.nextInt();
+                rowSize= myReader.nextInt();
+                myReader.nextLine();
+            }
+            ducks=new int[duckNumber][2];//ktora kacza, jaka wartosc
+            int tempI=0;
+            while(myReader.hasNextLine()){
+                ducks[tempI][0]=myReader.nextInt();
+                ducks[tempI][1]=myReader.nextInt();
+                tempI++;//change to for loop
+                myReader.nextLine();
+            }
+            myReader.close();//end function after this loop
+            for(int singleDuck[]: ducks){
+                System.out.println(singleDuck[0]+" "+singleDuck[1]);
+            }
+            doTheCalculations(ducks,duckNumber, rowSize);
+        }catch(FileNotFoundException e){
+            System.out.println("An error occured");
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean extractValueAtPosition(int intRepresentation,int position){
+        return ((intRepresentation) & (1 << (position))) !=0;
+    }
+
+    public static void doTheCalculations(int ducks[][], int duckNumber, int rowSize){
+        int bestSumHeights=0;
+        int bestMask=0;
+        int mask=0;
+        int bigNumber=(int)Math.pow(2,duckNumber);
+        while(mask<bigNumber){
+            int tempSumHeights=0;
+            int tempSumCost=0;
+            boolean validMask=true;
+
+            for(int i=0;i<duckNumber;i++){
+                if(extractValueAtPosition(mask,i)){
+                    if(tempSumCost+ducks[i][1]>rowSize){
+                        validMask=false;
+                        break;
+                    }
+                    tempSumHeights+=ducks[i][0];
+                    tempSumCost+=ducks[i][1];
+                }
+            }
+
+            if(bestSumHeights<tempSumHeights && validMask){
+                bestSumHeights=tempSumHeights;
+                bestMask=mask;
+            }
+
+            mask++;
+        }
+        System.out.println("Best sum is : "+bestSumHeights);
+        System.out.println("Mask is : "+bestMask);
+    }
+
+
+
+    public static void main(String[] args) {
+        readExcelData();//brut force ;/
+    }
+}
